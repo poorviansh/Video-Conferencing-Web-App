a10f4d773c304bd42d9ba4c9a933afa3eb4b4a40
Poorvi chaudhary <52835726+poorviansh@users.noreply.github.com>
2021-08-15 16:33:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = Process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000 ;
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
